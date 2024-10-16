0c11466d6267e015db10bab679ddaa6a4e219bd0
Kristóf Sajtos <kristof.sajtos@gmail.com>
2024-10-16 14:21:53 +0200

    CorrectedBugfix

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -30,18 +30,18 @@ public class TorpedoStore {
 
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
-      throw new IllegalArgumentException("numberOfTorpedos"); //nem volt throw eredetielg
+      new IllegalArgumentException("numberOfTorpedos");
     }
 
     boolean success = false;
 
     // simulate random overheating of the launcher bay which prevents firing
-    Random generator = new Random(); 
+    Random generator = new Random();
     double r = generator.nextDouble();
 
     if (r >= FAILURE_RATE) {
       // successful firing
-      this.torpedoCount -= numberOfTorpedos; //fordítva volt a -=
+      this.torpedoCount =- numberOfTorpedos;
       success = true;
     } else {
       // simulated failure
